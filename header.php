9788c94e004b57e6bc18f0b75c62a7076f9bc585
Stefon <52985099+StefonGlover@users.noreply.github.com>
2022-04-18 21:43:16 -0400

    Initial changes

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Fifteen Puzzle</title>
+    <meta charset="utf-8" />
+
+    <link rel="stylesheet" type="text/css" href="project.css" />
+
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" type="text/javascript"></script>
+    <script src="project.js" type="text/javascript"></script>
+</head>
+
+<body>
+    <nav>
+        <div class="logo">
+            <p>Fifteen Puzzle</p>
+        </div>
+    </nav>
